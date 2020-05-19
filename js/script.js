ead8e84162fad578542e40bbde3c51e11eac835e
Aldo Cahuana <aldooow@icloud.com>
2020-05-19 16:18:41 +0200

    Complete!!!!

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito.
+var nome = prompt('Qual è il tuo nome?');
+var cognome = prompt('Qual è il tuo cognome?');
+var colore = prompt('Qual è il tuo colore preferito?');
+
+
+document.getElementById('text').innerHTML = nome + cognome + colore + 19;
